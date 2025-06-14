86d94a2daa9a231c40088207e815fbf887a4f456
sundar pirabu raj r <sundarpiraburajr@sundars-MacBook-Air.local>
2025-06-14 14:34:12 +0530

    Initial commit.

---
+++ src/main/java/com/vrdoerz/userservice/VrdoerzUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vrdoerz.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrdoerzUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrdoerzUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrdoerz/userservice/VrdoerzUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrdoerz.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrdoerzUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
